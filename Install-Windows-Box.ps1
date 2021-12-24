2b0759405fb51b479fa5a41285151142355d4ffd
Michael Akinde <michael.work@akinde.dk>
2021-12-24 09:35:18 +0100

    Initial commit

---
+++ Install-Windows-Box.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+Installer/Upgrade Script for Windows Home PC
+
+.DESCRIPTION
+This script installs and upgrades applications and utilities for a Windows home PC
+
+.EXAMPLE
+.\Install-Windows-Box.ps1
+
+.INPUTS
+None
+
+.OUTPUTS
+None
+
+.LINK
+https://github.com/michaeloa/windows-box
+
+#>
+
+Write-Verbose "Installing Windows Box..."
